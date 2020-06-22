a07384ea26f0fb7c7a5787129badf6416c2060fa
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-06-22 16:10:15 -0600

    Add files via upload

--- Assignment 6/WorkFlow.cs
+++ Assignment 6/WorkFlow.cs
@@ -9,13 +9,30 @@ namespace Assignment_6.workflow
 {
     public class WorkFlow : IWorkFlow
     {
+        #region Properties
+        /// <summary>
+        /// a readonly list of type IActivity that stores the activies registered to be used via the workflow/workflow engine.
+        /// </summary>
         private readonly List<IActivity> registeredActivities;
+        #endregion
 
+        #region Constructor
+        /// <summary>
+        /// The Constructor for class WorkFlow, which implements the registerActivities list in order to track the count of the list, 
+        /// the ability to add to the list, as well as holding various activity information within the list.
+        /// </summary>
         public WorkFlow()
         {
             registeredActivities = new List<IActivity>();
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// Adds the activity specified in the method parameter to the "registeredActivities" list, 
+        /// and then sorts the list after adding to ensure the activity occurs in proper sequence.
+        /// </summary>
+        /// <param name="activity">The activity being passed into the workflow</param>
         public void RegisterActivity(IActivity activity)
         {
             if(activity == null)
@@ -29,14 +46,23 @@ namespace Assignment_6.workflow
                 registeredActivities.Sort();
         }
 
+        /// <summary>
+        /// Method using the IEnumerable in order to be able to list out the various activities stored in the "registerActivities" list via foreach statement.
+        /// </summary>
+        /// <returns>Returns the information for each activity contained in the list, in this case that is the console.writeline statements.</returns>
         public IEnumerable<IActivity> ActivitiesList()
         {
             return registeredActivities;
         }
 
+        /// <summary>
+        /// Counts the amount of activities currently stored within the registeredActivities list.
+        /// </summary>
+        /// <returns>returns the int amount of activities stored in the list.</returns>
         public int CountActivities()
         {
             return registeredActivities.Count;
         }
+        #endregion
     }
 }
--- Assignment 6/WorkFlowEngine.cs
+++ Assignment 6/WorkFlowEngine.cs
@@ -9,6 +9,12 @@ namespace Assignment_6.workflowengine
 {
     public class WorkFlowEngine : IWorkFlowEngine
     {
+        #region Methods
+        /// <summary>
+        /// Runs throw the workflow and executes the various activities contained in a presorted list 
+        /// using the execute logic/commands/console.writeline for each activity in the list.
+        /// </summary>
+        /// <param name="workFlow">The workflow containing a list of activities, along with theire specified sequences, to be ran by the workflowengine</param>
         public void Run(IWorkFlow workFlow)
         {       
             foreach (var activity in workFlow.ActivitiesList())
@@ -25,5 +31,6 @@ namespace Assignment_6.workflowengine
                 }              
             }
         }
+        #endregion
     }
 }
