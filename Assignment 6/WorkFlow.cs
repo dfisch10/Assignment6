e77f5a94f7e3663c8c9726786680c0dbac754620
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-06-21 22:42:08 -0600

    Add files via upload

---
+++ Assignment 6/WorkFlow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assignment_6.activities;
+using Assignment_6.interfaces;
+using Microsoft.VisualBasic;
+
+namespace Assignment_6.workflow
+{
+    public class WorkFlow : IWorkFlow
+    {
+        private readonly List<IActivity> registeredActivities;
+
+        public WorkFlow()
+        {
+            registeredActivities = new List<IActivity>();
+        }
+
+        public void RegisterActivity(IActivity activity)
+        {
+            if(activity == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You have tried to register a null activity, please enter an activity for this instance, and try again.");
+                Console.ResetColor();
+                return;
+            }
+                registeredActivities.Add(activity);
+                registeredActivities.Sort();
+        }
+
+        public IEnumerable<IActivity> ActivitiesList()
+        {
+            return registeredActivities;
+        }
+
+        public int CountActivities()
+        {
+            return registeredActivities.Count;
+        }
+    }
+}
---
+++ Assignment 6/WorkFlowEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Assignment_6.workflow;
+using Assignment_6.workflowengine;
+using Assignment_6.interfaces;
+using Assignment_6.activities;
+using System.Threading;
+
+namespace Assignment_6.workflowengine
+{
+    public class WorkFlowEngine : IWorkFlowEngine
+    {
+        public void Run(IWorkFlow workFlow)
+        {       
+            foreach (var activity in workFlow.ActivitiesList())
+            {
+                int count = 1;
+
+                while(activity.sequence >= count)
+                {
+                    if (activity.sequence == count)
+                    {
+                        activity.Execute();
+                    }
+                    count++;
+                }              
+            }
+        }
+    }
+}
